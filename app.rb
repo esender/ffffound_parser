77f117ddda687428949f4fd81c0d8ff0dfb66b78
Marat Abdulin <esend.work@gmail.com>
2014-02-01 13:51:39 +0400

    add db connection

--- app.rb
+++ app.rb
@@ -1,12 +1,35 @@
 require 'nokogiri'
 require 'open-uri'
+require 'sqlite3'
 
-pages = 1..100
+db = SQLite3::Database.new( "ffffound.db" )
 
-start_page = 'http://ffffound.com/'
+# rows = db.execute <<-SQL
+#   create table images (
+#     id integer primary key,
+#     image varchar(255),
+#     posted boolean default false
+#   );
+# SQL
 
-doc = Nokogiri::HTML(open(start_page))
+db.execute("INSERT INTO images (image) 
+            VALUES (?)", ['http://blablabal.ru'])
+# pages = 0..100
 
-doc.css("a img[@id*='asset']").each do |link|
-  puts link['src']
-end
+# start_page = 'http://ffffound.com/?offset='
+
+# pages.each do |index|
+#   doc = Nokogiri::HTML(open(start_page+(index*25).to_s))
+
+#   images = []
+
+#   doc.css("a img[@id*='asset']").each do |link|
+#     images << link['src']
+#   end
+
+#   puts images
+#   puts images.count
+#   p '------------------------------------------------------'
+#   sleep 1
+# end
+
