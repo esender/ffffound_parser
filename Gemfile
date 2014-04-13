70afe65b7836cb41d18a5b51d88e7e4dc207061b
vaihtovirta <5o.smoker@gmail.com>
2014-04-14 00:05:20 +0400

    changed group id

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 
 gem 'sqlite3'
 gem 'nokogiri'
-#gem 'active_support'
+gem 'activesupport', '~> 4.1.0'
 gem 'vkontakte_api', '~> 1.3'
 gem 'mechanize'
--- vk_poster.rb
+++ vk_poster.rb
@@ -13,29 +13,29 @@ temp_pic = 'temp/1.gif'
 agent = Mechanize.new
 db = SQLite3::Database.new( "ffffound.db" )
 
-db.execute( "select * from images WHERE id IS 43 ORDER BY created_at ASC LIMIT 10" ) do |row|
+db.execute( "select * from images WHERE id IS 44 ORDER BY created_at ASC LIMIT 10" ) do |row|
 	if row[1].include? gif
     agent.get(row[1]).save 'temp/pic.gif'
     temp_pic = 'temp/pic.gif'
-    url = vk.docs.get_wall_upload_server(group_id: 65617127)
+    url = vk.docs.get_wall_upload_server(group_id: 69899944)
     p url
     #upl = VkontakteApi.upload(url: url[:upload_url], photo: [temp_pic, 'image/gif'])
     #p upl
-    #post_item = vk.docs.save(upl.merge({gid:65617127}))
-    #vk.wall.post(owner_id: -65617127, attachments: post_item.map(&:id).join(','))
+    #post_item = vk.docs.save(upl.merge({gid:69899944}))
+    #vk.wall.post(owner_id: -69899944, attachments: post_item.map(&:id).join(','))
   else
     #agent.get(row[1]).save 'temp/pic.jpg'
     #temp_pic = 'temp/pic.jpg'
-    #url = vk.photos.get_wall_upload_server(group_id: 65617127)
+    #url = vk.photos.get_wall_upload_server(group_id: 69899944)
     #upl = VkontakteApi.upload(url: url[:upload_url], photo: [temp_pic, 'image/jpeg'])
-    #post_item = vk.photos.save_wall_photo(upl.merge({gid:65617127}))
-    #vk.wall.post(owner_id: -65617127, attachments: post_item.map(&:id).join(','))
+    #post_item = vk.photos.save_wall_photo(upl.merge({gid:69899944}))
+    #vk.wall.post(owner_id: -69899944, attachments: post_item.map(&:id).join(','))
   end
   #db.execute( "UPDATE images SET posted = true WHERE id = #{row[0]}")
   #File.delete(temp_pic)
 end
 
-#url = vk.docs.get_wall_upload_server(group_id: 65617127)
+#url = vk.docs.get_wall_upload_server(group_id: 69899944)
 #upl = VkontakteApi.upload(url: url[:upload_url], photo: [temp_pic, 'image/gif'])
-#p post_item = vk.docs.save_wall_photo(upl.merge({gid:65617127}))
-#vk.wall.post(owner_id: -65617127, attachments: post_item.map(&:id).join(','))
+#p post_item = vk.docs.save_wall_photo(upl.merge({gid:69899944}))
+#vk.wall.post(owner_id: -69899944, attachments: post_item.map(&:id).join(','))
